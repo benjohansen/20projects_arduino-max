a50d2214fd2557d26d3ba6ef4947937ef9ffea1c
Ben Johansen <b_johansen@baylor.edu>
2022-02-01 10:49:53 -0600

    blink updates

---
+++ blink/README.md
@@ -0,0 +1,2 @@
+# blink
+Make the LED on pin 13 of the Digital Sandbox blink by using a [metro], [toggle], and [serial] object in Max.
--- blink/blink.ino
+++ blink/blink.ino
@@ -1,17 +1,19 @@
+// this basically allows you, in Max, to send a toggle button to the serial object
+
 const int ledPin = 13;      // the pin that the LED is attached to
-int maxData = 0;            // byte sent from max
+int maxData = 0;            // byte sent from Max
 
 void setup()
   {
-    Serial.begin(9600);     // init the serial port
-    pinMode(ledPin, OUTPUT);   // make pin 13 an output
+    Serial.begin(9600);        // opens serial port, sets data rate to 9600 bps
+    pinMode(ledPin, OUTPUT);   // set pin 13 as an output pin
   }
 
 void loop()
   {
-    while(Serial.available() > 0) {
-      maxData = Serial.read();
+    while(Serial.available() > 0) {   // as long as there is data in the serial buffer ...
+      maxData = Serial.read();        // ... read the data
     }
-    if(maxData == 1)digitalWrite(ledPin, HIGH);   // turn led on
-    if(maxData == 0)digitalWrite(ledPin, LOW);    // turn led off
+    if(maxData == 1)digitalWrite(ledPin, HIGH);   // turn led on if Max sent a 1
+    if(maxData == 0)digitalWrite(ledPin, LOW);    // turn led off if Max sent a 0
   }
