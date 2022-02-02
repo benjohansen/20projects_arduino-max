d5b0c10ff978e4fb8f3e2a54372e4bc712a73582
Ben Johansen <b_johansen@baylor.edu>
2022-02-02 14:19:57 -0600

    Update to-and-from-max.ino

--- to-and-from-max/to-and-from-max.ino
+++ to-and-from-max/to-and-from-max.ino
@@ -1,15 +1,14 @@
+//constants (i.e. they won't change) to store LED RGB pins
+const int analogInPin = A3;  // Analog input pin that the potentiometer is attached to
+const int redPin = 9;        // pins for the RGB LED
+const int greenPin = 10;
+const int bluePin = 11;
+
 //variables to store RGB values
 int sensorValue = 0;  //value read from the analog in pin
 int redVal = 0;
 int greenVal = 0;
 int blueVal = 0;
-
-//constants (i.e. they won't change) to store LED RGB pins
-const int analogInPin = A3;  // Analog input pin that the potentiometer is attached to
-const int redPin = 9;
-const int greenPin = 10;
-const int bluePin = 11;
-
 
 
 void setup() {
@@ -26,23 +25,19 @@ pinMode(bluePin, OUTPUT);
 void loop() {
   if (Serial.available() > 0) {
   
-  
-  
-  //parse (split) the incoming list of 3 values
-  //and store them in the variables
+  //parse (split) the incoming list of 3 values and store them in specific variables
   redVal = Serial.parseInt();
   greenVal = Serial.parseInt();
   blueVal = Serial.parseInt();
   
-  //write out the values to the LED pins
+  //send the specified amount of voltage to the LED pins
   analogWrite(redPin, redVal);
   analogWrite(greenPin, greenVal);
   analogWrite(bluePin, blueVal);    
 
-    
-
   sensorValue = analogRead(analogInPin);  // read the analog in value
+  
   Serial.println(sensorValue); // print the results to the serial monitor
-  //delay(10); // wait 10 ms before the next loop for the ADC to settle after the last reading
+  
   }
 }
